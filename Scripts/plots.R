fd1f2b1f7b15b159b2cb3736ef793d0f988f6cac
Shawn Dsouza <shawn27.dsouza@gmail.com>
2021-06-08 23:55:51 +0530

    Updated stats reporting and plots

--- Scripts/plots.R
+++ Scripts/plots.R
@@ -19,8 +19,8 @@ fig2a <-HS%>%
   geom_segment(data = births, aes(x = Month, xend = Month, y = 0 , yend = 10), #marking births
                arrow = arrow(length = unit(0.25, "cm"), ends = "first"), size = 1)+
   geom_text(data = births, aes(x = Month, y = 20, label = paste("Observed \n births")))+
-  geom_vline(aes(xintercept = "June"), size = 1)+#start of the monsoon ban
-  geom_vline(aes(xintercept = "August"), size = 1)+#end of the monsoon ban
+  geom_vline(aes(xintercept = "June"), size = 1, linetype = "dotted")+#start of the monsoon ban
+  geom_vline(aes(xintercept = "August"), size = 1, linetype = "dotted")+#end of the monsoon ban
   geom_text(aes(x = "July", y = 80, label = "Monsoon ban"))+
   scale_x_discrete(guide = guide_axis(n.dodge = 2))+
   labs(y = "Snout to vent length (cm)", title = "A")+
@@ -30,20 +30,20 @@ fig2b <- gravid%>%
   mutate(Month = factor(Month, levels = month.name))%>%
   complete(Month, fill = list(prop.gravid = 0))%>%
   ggplot(aes(Month, prop.gravid))+
-  geom_point(size = 3)+
-  geom_path(aes(group = 1), size = 1, linetype = "dashed")+
+  geom_pointrange(size = 1, aes(ymin = prop.gravid + sd, ymax = prop.gravid - sd))+
+  geom_path(aes(group = 1), size = 0.5, linetype = "dashed")+
   geom_segment(aes(x = "April", xend = "April", y = 0 , yend = 0.02), #marking births
                arrow = arrow(length = unit(0.25, "cm"), ends = "first"), size = 1)+
   geom_text(aes(x = "April", y = 0.04, label = paste("Observed \n births")))+
-  geom_vline(aes(xintercept = "June"), size = 1)+#start of the monsoon ban
-  geom_vline(aes(xintercept = "August"), size = 1)+#end of the monsoon ban
+  geom_vline(aes(xintercept = "June"), size = 1, linetype = "dotted")+#start of the monsoon ban
+  geom_vline(aes(xintercept = "August"), size = 1, linetype = "dotted")+#end of the monsoon ban
   geom_text(aes(x = "July", y = 0.20, label = "Monsoon ban"))+
   scale_x_discrete(guide = guide_axis(n.dodge = 2))+
   labs(y = "Proportion of gravid females", title = "B")
 
 fig2 <- gridExtra::grid.arrange(fig2a, fig2b, ncol = 1)
 
-ggsave(fig2, filename = "../Figures/figure2.tiff", height = 9, width = 8)
+ggsave(fig2, filename = "./Figures/figure2.tiff", height = 9, width = 8)
 
 # Figure 3
 
